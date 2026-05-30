0a16eb57f00dec614c59272e114b4ae778c574e6
vikanadolinskay <vikanadol@gmail.com>
2026-05-30 13:37:15 +0300

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { SafeAreaView, StatusBar } from 'react-native';
+import AppNavigator from './src/navigation';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return ( <
+        >
+        <
+        StatusBar barStyle = "light-content"
+        backgroundColor = "#D2691E" / >
+        <
+        SafeAreaView style = {
+            { flex: 1, backgroundColor: '#D2691E' } } >
+        <
+        AppNavigator / >
+        <
+        /SafeAreaView> <
+        />
+    );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
